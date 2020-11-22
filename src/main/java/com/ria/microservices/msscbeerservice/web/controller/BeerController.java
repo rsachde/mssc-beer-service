2973a266937f0640aceaddcb446b35a789eeb35f
Ria <riasachdeva2015@gmail.com>
2020-11-22 01:06:33 -0500

    Adding Test Cases. Closes #3

---
+++ src/main/java/com/ria/microservices/msscbeerservice/web/controller/BeerController.java
@@ -0,0 +1,36 @@
+package com.ria.microservices.msscbeerservice.web.controller;
+
+import com.ria.microservices.msscbeerservice.web.model.BeerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RequestMapping("api/v1/beer")
+@RestController
+public class BeerController {
+
+
+    @GetMapping("/{beerId}")
+    public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId)
+    {
+      //todo implementation
+        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto)
+    {
+        //todo implementation
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+    @PutMapping("/{beerId}")
+    public ResponseEntity updateBeerById(@PathVariable("beerID") UUID beerId, @RequestBody BeerDto beerDto)
+    {
+
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+
+}
