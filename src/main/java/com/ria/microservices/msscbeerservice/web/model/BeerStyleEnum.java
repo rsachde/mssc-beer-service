148c1ce2dbdd84ce4c81a460113b90e2490c92e8
Ria <riasachdeva2015@gmail.com>
2020-11-21 23:53:34 -0500

    Adding Web Data Model. Closes #2

---
+++ src/main/java/com/ria/microservices/msscbeerservice/web/model/BeerStyleEnum.java
@@ -0,0 +1,15 @@
+package com.ria.microservices.msscbeerservice.web.model;
+
+public enum BeerStyleEnum {
+    LARGER,
+    PILSNER,
+    STOUT,
+    GOSE,
+    PORTER,
+    ALE,
+    WHEAT,
+    IPA,
+    PALE_ALE,
+    SAISON
+}
+
